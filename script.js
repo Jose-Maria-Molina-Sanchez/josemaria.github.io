35c7bc40f554582e460b6ef9e2806faae1a8b582
Jose-Maria-Molina-Sanchez <116257065+Jose-Maria-Molina-Sanchez@users.noreply.github.com>
2025-10-26 12:19:26 +0100

    Mejora de mi página personal

--- script.js
+++ script.js
@@ -11,9 +11,33 @@ document.addEventListener('DOMContentLoaded', function() {
         window.open('https://github.com/Jose-Maria-Molina-Sanchez', '_blank');
     });
 
-    // Botón Contacto - enlace a index.html
-    document.getElementById('contact').addEventListener('click', function(e) {
+    // Botón Inicio - enlace a index.html
+    document.getElementById('inicio').addEventListener('click', function(e) {
         e.preventDefault();
         window.location.href = 'index.html';
+    });
+
+    // Botón Sobre Mí - enlace a yo.html
+    document.getElementById('yo').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.href = 'yo.html';
+    });
+
+    // Botón Sobre Mis PRoyectos - enlace a proyectos.html
+    document.getElementById('proyectos').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.href = 'proyectos.html';
+    });
+
+    // Botón Sobre Formulario - enlace a https://jose-maria-molina-sanchez.github.io/formulario.github.io/
+    document.getElementById('formulario').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.open('https://jose-maria-molina-sanchez.github.io/formulario.github.io/');
+    });
+
+    // Botón Sobre Jardineria - enlace a https://jose-maria-molina-sanchez.github.io/formulario.github.io/
+    document.getElementById('jardineria').addEventListener('click', function(e) {
+        e.preventDefault();
+        window.open('https://github.com/Jose-Maria-Molina-Sanchez/Proyecto-Jardineria-CSharp');
     });
 });
